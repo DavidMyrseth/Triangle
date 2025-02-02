bf0db19d48ad064c68ff69a29ac325d966cc0524
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2025-02-02 19:37:58 +0200

    gg

---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Triangle_vorm
+{
+    public partial class Form1 : Form
+    {
+        Button startBtn;
+        ListView listView;
+        TextBox textBox1, textBox2, textBox3;
+        Label label1,label2,label3;
+        PictureBox pictureBox;
+
+        public Form1()
+        {
+            Width = 800;
+            Height = 400;
+            Text = "Töö kolmnurgaga";
+
+            startBtn = new Button();
+            startBtn.Text = "Käivita";
+            startBtn.Size = new Size(100, 60);
+            startBtn.Location = new Point(600,40);
+            startBtn.BackColor = Color.CornflowerBlue;
+            startBtn.Font = new Font("Arial",15,FontStyle.Bold);
+            startBtn.Cursor = Cursors.Hand;
+            startBtn.FlatAppearance.BorderColor = Color.Aqua;
+            startBtn.FlatAppearance.BorderSize = 4;
+            startBtn.FlatStyle = FlatStyle.Flat;
+            startBtn.Click += Run_button_click;
+            startBtn.MouseHover += OpenSecondFormBtn_Click;
+            Controls.Add(startBtn);
+
+            listView = new ListView();
+            listView.Size = new Size(350,200);
+            listView.Location = new Point(50,30);
+            listView.View = View.Details;
+            listView.Columns.Add("Väli", 175 ,(int)HorizontalAlignment.Left);
+            listView.Columns.Add("Väärtus", 175 ,(int)HorizontalAlignment.Left);
+
+            Controls.Add(listView);
+            
+            label1 = new Label();
+            label1.Text = "Külg a";
+            label1.Size = new Size(50,17);
+            label1.Location = new Point(50, 230);
+            Controls.Add(label1);
+
+            textBox1 = new TextBox();
+            textBox1.Size = new Size(100, 40);
+            textBox1.Location = new Point(50,250);
+            Controls.Add(textBox1);
+
+            label2 = new Label();
+            label2.Text = "Külg b";
+            label2.Size = new Size(50, 17);
+            label2.Location = new Point(50, 270);
+            Controls.Add(label2);
+
+            textBox2 = new TextBox();
+            textBox2.Size = new Size(100, 40);
+            textBox2.Location = new Point(50, 290);
+            Controls.Add(textBox2);
+
+            label3 = new Label();
+            label3.Text = "Külg c";
+            label3.Size = new Size(50, 17);
+            label3.Location = new Point(50, 310);
+            Controls.Add(label3);
+
+            textBox3 = new TextBox();
+            textBox3.Size = new Size(100, 40);
+            textBox3.Location = new Point(50, 330);
+            Controls.Add(textBox3);
+
+            pictureBox = new PictureBox();
+            pictureBox.Size = new Size(200, 200); 
+            pictureBox.Location = new Point(450, 140); 
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage; 
+            Controls.Add(pictureBox);
+
+        }
+
+        private void OpenSecondFormBtn_Click(object sender, EventArgs e)
+        {
+            Form2 secondForm = new Form2();
+            secondForm.Show();
+        }
+        private void Run_button_click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            bool inputA = double.TryParse(textBox1.Text, out a);
+            bool inputB = double.TryParse(textBox2.Text, out b);
+            bool inputC = double.TryParse(textBox3.Text, out c);
+
+            // Clear the ListView before adding new items
+            listView.Items.Clear();
+
+            listView.Items.Add("Külg a");
+            listView.Items.Add("Külg b");
+            listView.Items.Add("Külg c");
+            listView.Items.Add("Perimeeter");
+            listView.Items.Add("Pindala");
+            listView.Items.Add("On olemas?");
+            listView.Items.Add("Nurk A");
+            listView.Items.Add("Nurk B");
+            listView.Items.Add("Nurk C");
+            listView.Items.Add("Kolmnurga tüüp");
+
+            if (inputA && inputB && inputC)
+            {
+                if (a <= 0)
+                {
+                    MessageBox.Show("Külg a peab olema positiivne väärtus.");
+                    return;
+                }
+
+                if (b <= 0)
+                {
+                    MessageBox.Show("Külg b peab olema positiivne väärtus.");
+                    return;
+                }
+
+                if (c <= 0)
+                {
+                    MessageBox.Show("Külg c peab olema positiivne väärtus.");
+                    return;
+                }
+
+                Triangle triangle = new Triangle(a, b, c);
+
+                triangle.CalculateNurgCos();
+
+                listView.Items[0].SubItems.Add(triangle.GetSetA.ToString());
+                listView.Items[1].SubItems.Add(triangle.GetSetB.ToString());
+                listView.Items[2].SubItems.Add(triangle.GetSetC.ToString());
+
+                listView.Items[3].SubItems.Add(triangle.Perimeter().ToString());
+                listView.Items[4].SubItems.Add(triangle.Surface().ToString());
+
+                if (triangle.ExistTrtiangle)
+                {
+                    listView.Items[5].SubItems.Add("On olemas");
+                    SaveTriangleToXml(triangle);
+                }
+                else
+                {
+                    listView.Items[5].SubItems.Add("Ei ole olemas");
+                }
+
+                listView.Items[6].SubItems.Add(triangle.outputNurgA());
+                listView.Items[7].SubItems.Add(triangle.outputNurgB());
+                listView.Items[8].SubItems.Add(triangle.outputNurgC());
+
+                listView.Items[9].SubItems.Add(triangle.TriangleType);
+
+                if (triangle.TriangleType.Contains("Võrdkülgne"))
+                {
+                    pictureBox.Image = Image.FromFile(@"Võrdkülgne.png");
+                }
+                else if (triangle.TriangleType.Contains("Võrdhaarane"))
+                {
+                    pictureBox.Image = Image.FromFile(@"Võrdhaarane.png");
+                }
+                else if (triangle.TriangleType.Contains("Erikülgne"))
+                {
+                    pictureBox.Image = Image.FromFile(@"Erikülgne.gif");
+                }
+                else
+                {
+                    pictureBox.Image = null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Palun sisestage numbrid.");
+            }
+        }
+
+        private void SaveTriangleToXml(Triangle triangle)
+        {
+            string filePath = "kolmnurgad.xml";
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            if (!File.Exists(filePath))
+            {
+                XmlElement root = xmlDoc.CreateElement("Triangles");
+                xmlDoc.AppendChild(root);
+            }
+            else
+            {
+                try
+                {
+                    xmlDoc.Load(filePath);
+                }
+                catch (XmlException)
+                {
+                    XmlElement root = xmlDoc.CreateElement("Triangles");
+                    xmlDoc.AppendChild(root);
+                }
+            }
+
+            XmlElement triangleElement = xmlDoc.CreateElement("Triangle");
+
+            triangleElement.SetAttribute("KülgA", triangle.GetSetA.ToString());
+            triangleElement.SetAttribute("KülgB", triangle.GetSetB.ToString());
+            triangleElement.SetAttribute("KülgC", triangle.GetSetC.ToString());
+            triangleElement.SetAttribute("Perimeeter", triangle.Perimeter().ToString());
+            triangleElement.SetAttribute("Pindala", triangle.Surface().ToString());
+            triangleElement.SetAttribute("NurkA", triangle.outputNurgA().ToString());
+            triangleElement.SetAttribute("NurkB", triangle.outputNurgB().ToString());
+            triangleElement.SetAttribute("NurkC", triangle.outputNurgC().ToString());
+            triangleElement.SetAttribute("KolmnurgaTüüp", triangle.TriangleType);
+
+            xmlDoc.DocumentElement.PrependChild(triangleElement);
+
+            xmlDoc.Save(filePath);
+        }
+
+        //private void SaveTriangleToXml(Triangle triangle)
+        //{
+        //    string filePath = "kolmnurgad.xml";
+
+        //    XmlWriterSettings settings = new XmlWriterSettings();
+        //    settings.Indent = true;
+
+        //    XmlWriter writer = XmlWriter.Create(filePath, settings);
+
+        //    writer.WriteStartDocument();
+        //    writer.WriteStartElement("Triangles");
+
+        //    writer.WriteStartElement("Triangle");
+
+        //    writer.WriteAttributeString("KülgA", triangle.GetSetA.ToString());
+        //    writer.WriteAttributeString("KülgB", triangle.GetSetB.ToString());
+        //    writer.WriteAttributeString("KülgC", triangle.GetSetC.ToString());
+        //    writer.WriteAttributeString("Perimeeter", triangle.Perimeter().ToString());
+        //    writer.WriteAttributeString("Pindala", triangle.Surface().ToString());
+        //    writer.WriteAttributeString("NurkA", triangle.outputNurgA().ToString());
+        //    writer.WriteAttributeString("NurkB", triangle.outputNurgB().ToString());
+        //    writer.WriteAttributeString("NurkC", triangle.outputNurgC().ToString());
+        //    writer.WriteAttributeString("KolmnurgaTüüp", triangle.TriangleType);
+
+        //    writer.WriteEndElement();
+        //    writer.WriteEndElement();
+
+        //    writer.Flush();
+
+        //    writer.Close();
+
+        //}
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_vorm
+{
+    internal class Triangle
+    {
+        private double a, b, c, h;
+        private double nurkA, nurkB, nurkC;
+
+        public Triangle() { }
+
+        public Triangle(double A) 
+        {
+            a = A;
+            b = A;
+            c = A;
+        }
+
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public Triangle(double side, double height)
+        {
+            this.h = height;
+            a = side;         
+        }
+
+        public double CalculateSurfaceHeight()
+        {
+            return 0.5 * a * h;
+        }   
+
+        public string outputA()
+        {
+            return Convert.ToString(c);
+        }
+
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+
+        public string outputH()
+        {
+            return Convert.ToString(h);
+        }       
+
+        public string outputNurgA() 
+        {
+            return nurkA.ToString("F2"); ;
+        }
+
+        public string outputNurgB()
+        {
+            return nurkB.ToString("F2"); ;
+        }
+
+        public string outputNurgC()
+        {
+            return nurkC.ToString("F2"); ;
+        }
+
+        // https://stackoverflow.com/questions/45791505/find-angle-from-given-side-of-right-andgle-triangle-and-vise-versa-in-c-sharp
+
+        public void CalculateNurgCos()
+        {
+            if (ExistTrtiangle)
+            {
+                nurkA = Math.Acos((b * b + c * c - a * a) / (2 * b * c)) * (180 / Math.PI);
+                nurkB = Math.Acos((a * a + c * c - b * b) / (2 * a * c)) * (180 / Math.PI);
+                nurkC = Math.Acos((a * a + b * b - c * c) / (2 * a * b)) * (180 / Math.PI);
+            }
+            else
+            {
+                nurkA = 0;
+                nurkB = 0;
+                nurkC = 0;
+            }
+        }
+
+        public double Perimeter()
+        {
+            if (ExistTrtiangle)
+            {
+                return a + b + c;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double Surface()
+        {
+            if (ExistTrtiangle)
+            {
+                double p = (a + b + c) / 2;  
+                double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s;
+            }
+            else 
+            { 
+                return 0;
+            }
+            
+        }
+
+        public double GetSetA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public double GetSetH
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public double GetSetNurgA
+        {
+            get { return nurkA; }
+            set { nurkA = value; }
+        }
+
+        public double GetSetNurgB
+        {
+            get { return nurkB; }
+            set { nurkB = value; }
+        }
+
+        public double GetSetNurgC
+        {
+            get { return nurkC; }
+            set { nurkC = value; }
+        }
+
+        public bool ExistTrtiangle
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+        public string TriangleType
+        {
+            get
+            {
+                if (ExistTrtiangle)
+                {
+                    string külg = "";
+                    if (a == b && b == c && a == c)
+                    {
+                        külg = "Võrdkülgne";
+                    }
+                    else if (a == b || a == c || b == c)
+                    {
+                        külg = "Võrdhaarane";
+                    }
+                    else
+                    {
+                        külg = "Erikülgne";
+                    }
+
+                    string nurg = "";
+                    if (nurkA < 90 && nurkB < 90 && nurkC < 90)
+                    {
+                        nurg = "Teravnurkne";
+                    }
+                    else if (nurkA == 90 || nurkB == 90 || nurkC == 90)
+                    {
+                        nurg = "Täisnurkne";
+                    }
+                    else
+                    {
+                        nurg = "Nürinurkne";
+                    }
+
+                    return $"{külg} ja {nurg} kolmnurk";
+                }
+                else
+                {
+                    return "Ei ole kolmnurk ";
+                }
+            }
+        }
+    }
+}
